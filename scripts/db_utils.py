41cd87f3742365312a87eb1c06677f998f0ea13d
smhooper <samhooperstudio@gmail.com>
2019-09-12 09:25:04 -0800

    add func to get lookup table as dict

--- scripts/db_utils.py
+++ scripts/db_utils.py
@@ -53,3 +53,33 @@ def get_db_columns(table_name, engine):
             .squeeze()
 
     return db_columns
+
+
+def get_lookup_table(engine, table, index_col='code', value_col='name'):
+    ''' Return a dictionary of code: name pairs from a given lookup table'''
+
+    LOOKUP_TABLES = {'destinations': 'destination_codes',
+                     'buses': 'bus_codes',
+                     'approved': 'nps_approved_codes',
+                     'work_groups': 'nps_work_groups'}
+
+    with engine.connect() as conn, conn.begin():
+        sql = "SELECT DISTINCT table_name FROM information_schema.tables WHERE table_schema = 'public';"
+        table_names = pd.read_sql(sql, conn).squeeze()
+
+        if table in LOOKUP_TABLES:
+            table = LOOKUP_TABLES[table]
+        if table in table_names.values:
+            data = pd.read_sql("SELECT * FROM %s" % table, conn)
+        else:
+            table_options = '\n\t\t'.join(sorted(table_names.tolist() + LOOKUP_TABLES.keys()))
+            raise ValueError('Table named "%s" not found. Options:\n\t\t%s"' % (table, table_options))
+
+    if index_col not in data.columns:
+        raise ValueError('index_col "%s" not found in table columns: %s' % (index_col, ', '.join(data.columns)))
+    if value_col not in data.columns:
+        raise ValueError('value_col "%s" not found in table columns: %s' % (value_col, ', '.join(data.columns)))
+
+    data.set_index(index_col, inplace=True)
+
+    return data[value_col].to_dict()
