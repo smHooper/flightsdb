69cdcaee462fad4ebe952f28294bc271bfeb0205
smhooper <samhooperstudio@gmail.com>
2019-09-27 10:55:25 -0800

    make script to update aircraft_info table

--- scripts/import_track.py
+++ scripts/import_track.py
@@ -59,6 +59,7 @@ from geoalchemy2 import Geometry, WKTElement
 from shapely.geometry import LineString as shapely_LineString, Point as shapely_Point
 
 import db_utils
+import get_missing_aircraft_info as ainfo
 import process_emails
 
 
@@ -401,33 +402,6 @@ def check_duplicate_flights(registration, connection, start_time, end_time):
     return matching_flights
 
 
-def get_aircraft_info(registration, ssl_cert):
-    """
-    Scrape the FAA registry for info about an aircraft from its registration (N-) number
-
-    :param registration: N-number of the aircraft
-    :param ssl_cert: path to an SSL .crt or .pem file
-    :return: pd.Series of aircraft info (index is the property name)
-    """
-
-    faa_url = 'https://registry.faa.gov/aircraftinquiry/NNum_Results.aspx?NNumbertxt={}'
-    response = requests.get(faa_url.format(registration),
-                            verify=ssl_cert)
-    response.raise_for_status() # exception should be caught when calling this function
-    soup = bs4.BeautifulSoup(response.text)
-
-    # remove special characters from what will be field names and strip trailing spaces from values
-    keys = [re.sub('[^\w0-9a-zA-Z]+', '_', k.getText()).lower() for k in soup.find_all(class_='Results_DataLabel')]
-    values = [v.getText().strip() for v in soup.find_all(class_='Results_DataText')]
-
-    # There should be the same number of keys and values since each piece of info (value) in the table has a label (key)
-    #   so it should work just to zip them up
-    info = pd.Series(zip(keys, values))
-    info['registration'] = registration
-
-    return info
-
-
 def import_track(connection_txt, path, seg_time_diff=15, min_point_distance=200, registration='', submission_method='manual', operator_code=None, aircraft_type=None, silent=False, force_import=False, ssl_cert_path=None):
 
     _, extension = os.path.splitext(path)
@@ -578,24 +552,7 @@ def import_track(connection_txt, path, seg_time_diff=15, min_point_distance=200,
 
         # INSERT info about this aircraft if it doesn't already exist. If it does, UPDATE it if necessary
         if ssl_cert_path:
-            existing_aircraft = pd.read_sql("SELECT * FROM aircraft_info;", conn)
-            aircraft_info = pd.Series() # initialize in case the try block fails
-            try:
-                aircraft_info = get_aircraft_info(registration, ssl_cert_path)
-            except Exception as e:
-                warnings.warn('Could not retrieve aircraft info because %s' % e)
-            if len(aircraft_info):
-                aircraft_info = aircraft_info.loc[[c for c in existing_aircraft.columns if c in aircraft_info]]
-                existing_info = existing_aircraft.loc[existing_aircraft.registration == registration]
-                if len(existing_info): # would be an empty df if the registration doesn't already exist
-                    # check if the record needs to be updated
-                    if not (existing_info == aircraft_info).all():
-                        sql = "UPDATE aircraft_info SET {columns} WHERE registration = '{registration}';"\
-                            .format(columns=', '.join(["%s = '%s'" % (k, v) for k, v in aircraft_info.iteritems()]),
-                                    registration=registration)
-                        conn.execute(sql)
-                else:
-                    pd.DataFrame([aircraft_info]).to_sql('aircraft_info', conn, if_exists='append', index=False)
+            ainfo.update_aircraft_info(conn, registration, ssl_cert_path)
 
     # Archive the data file
     if not os.path.isdir(ARCHIVE_DIR):
