e44aa03ff62f9e2e425faded0fd662c679a25d1b
smhooper <samhooperstudio@gmail.com>
2019-11-13 13:03:25 -0900

    process landing data; wrap polling function in context manager with connections

--- scripts/poll_feature_service.py
+++ scripts/poll_feature_service.py
@@ -2,7 +2,7 @@ import os
 import sys
 import json
 import re
-import pytz
+import zipfile
 import traceback
 import pandas as pd
 from glob import glob
@@ -12,17 +12,31 @@ from sqlalchemy import create_engine
 import download_feature_service as download
 import db_utils
 
+pd.set_option('display.max_columns', None)# useful for debugging
+
 SUBMISSION_TICKET_INTERVAL = 900 # seconds between submission of a particular user
 LOG_CACHE_DAYS = 14 # amount of time to keep a log file
 TIMESTAMP_FORMAT = '%Y-%m-%d %H:%M:%S'
 LANDING_FEE = 5.15 # per person fee for each passenger on scenic flight or dropped off
 
-LANDING_FLIGHT_COLUMNS = {'landing_operator': 'operator_code',
-                          'landing_tail_number': 'registration',
-                          'landing_datetime': 'departure_datetime',
-                          'landing_aircraft_type': 'aircraft_type'
+LANDINGS_FLIGHT_COLUMNS = {'landing_operator': 'operator_code',
+                           'landing_tail_number': 'registration',
+                           'landing_datetime': 'departure_datetime',
+                           'landing_route': 'scenic_route',
+                           'landing_aircraft_type': 'aircraft_type',
+                           'globalid': 'agol_global_id',
+                           'ticket': 'ticket',
+                           'submission_method': 'submission_method',
+                           'flight_id': 'flight_id',
+                           'landing_flight_notes': 'notes'
                           }
-
+LANDINGS_COLUMNS = {'flight_id': 'flight_id',
+                    'landing_location': 'location',
+                    'n_passengers': 'n_passengers',
+                    'landing_type': 'landing_type',
+                    'landing_justification': 'justification',
+                    'landing_notes': 'notes'
+                    }
 
 def write_log(log_dir, download_dir, timestamp, submission_time, data_was_downloaded, error):
 
@@ -68,28 +82,24 @@ def julian_date_to_datetime(julian_date):
         return pd.NaT
 
 
-def get_ticket(group, connection_json):
+def get_ticket(group, connections):
     '''
     Return a dataframe with a ticket number assigned to all records in a new column
     '''
 
-    with open(connection_json) as j:
-        connection_info = json.load(j)
-    submission_type = group.submission_type.iloc[0]# should be all the same submission type
-    engine = create_engine('postgresql://{username}:{password}@{ip_address}:{port}/{db_name}'
-                           .format(**connection_info[submission_type]))
-
+    submission_type = group.submission_type.iloc[0]  # should be all the same submission type
     submitter = group.submitter.iloc[0]
     submission_time = group.submission_time.min()
-    with engine.connect() as conn, conn.begin():
-        conn.execute("INSERT INTO submissions (submitter, submission_time) VALUES ('%s', '%s');" % (submitter, submission_time))
-        group['ticket'] = pd.read_sql("SELECT max(ticket) FROM submissions;", conn).squeeze()
+    conn = connections[submission_type]
+    conn.execute("INSERT INTO submissions (submitter, submission_time) VALUES ('%s', '%s');" % (submitter, submission_time))
+    group['ticket'] = pd.read_sql("SELECT max(ticket) FROM submissions;", conn).squeeze()
 
     return group
 
 
 def poll_feature_service(log_dir, download_dir, agol_credentials_json, email_credentials_json, db_credentials_json, ssl_cert, landings_conn, tracks_conn):
 
+    messages = []
 
     timestamp = datetime.now().strftime(TIMESTAMP_FORMAT)
 
@@ -147,18 +157,24 @@ def poll_feature_service(log_dir, download_dir, agol_credentials_json, email_cre
 
     # download the data
     sqlite_path = download.download_data(download_dir, token, layers, service_info, service_url, ssl_cert, last_poll_time=last_download_time)
-    engine = create_engine('sqlite:///' + sqlite_path)
+
+    # Make edits to a working copy of the data
+    working_dir = os.path.join(download_dir, 'working')
+    if not os.path.isdir(working_dir):
+        os.mkdir(working_dir)
+    working_sqlite_path = os.path.join(working_dir, os.path.basename(sqlite_path))
+    engine = create_engine('sqlite:///' + working_sqlite_path)
 
     # Create separate tickets for each user for both landing and track data
     submissions['submission_time'] = [datetime.fromtimestamp(ts/1000) for _, ts in submissions['CreationDate'].iteritems()]
     submissions.rename(columns={'Creator': 'submitter'}, inplace=True)
-    with open(db_credentials_json) as j:
-        connection_dict = json.load(j)
 
-    submissions = submissions.groupby(['submitter', 'submission_type'])\
-        .apply(lambda g: get_ticket(g, db_credentials_json))
+    connection_dict = {'tracks': tracks_conn, 'landings':landings_conn}
+    submissions = pd.concat([get_ticket(g, connection_dict)
+                             for _, g in submissions.groupby(['submitter', 'submission_type'])])
 
-    # Process and import the landing data. The flight tracks will need to be unzipped, but the track data need to be validated/edited before they can be imported. This is handled manually via a separate web app
+    # Process and import the landing data. The flight tracks will need to be unzipped, but they need to be
+    #   validated/edited before they can be imported. This is handled manually via a separate web app
     main_table_name = pd.DataFrame(service_info['layers']).set_index('id').loc[0, 'name']
     with engine.connect() as conn, conn.begin():
         # Convert datetimes in place and get rid of the braces around IDs
@@ -166,48 +182,62 @@ def poll_feature_service(log_dir, download_dir, agol_credentials_json, email_cre
             df = pd.read_sql_table(table_name, conn)
 
             for field, _ in data_types.loc[data_types == 'esriFieldTypeDate'].iteritems():
-                df[field] = df[field].apply(julian_date_to_datetime).apply(pd.Timestamp)
+                df[field] = pd.to_datetime([julian_date_to_datetime(jd) for jd in df[field]]).round('S')
 
             for field in df.columns[['globalid' in c.lower() for c in df.columns]]:
                 df[field] = df[field].str.replace('{', '').str.replace('}', '')
-            df.to_sql(table_name, conn, if_exists='replace')
+            df.to_sql(table_name, conn, if_exists='replace', index=False)
 
         # Add the ticket column to the sqlite flights table
         all_flights = pd.read_sql_table(main_table_name, conn)
-        '''landing_flights = pd.read_sql("SELECT * FROM %s WHERE submission_type = 'landings'" % main_table_name, conn)\
-            .dropna(axis=1, how='all') # columns for all track submissions will be empty so drop them
-        track_flights = pd.read_sql("SELECT * FROM %s WHERE submission_type = 'tracks'" % main_table_name, conn)\
-            .dropna(axis=1, how='all')'''
         # global IDs in query are lowercase but they're uppercase in createReplica
         submissions.globalid = submissions.globalid.str.upper()
         all_flights['ticket'] = all_flights.merge(submissions, on='globalid').ticket
         all_flights.to_sql(main_table_name, conn, if_exists='replace')
-        '''track_flights['ticket'] = track_flights.merge(submissions, on='globalid').ticket
-        track_flights.to_sql()
-
-        landing_flights['ticket'] = landing_flights.merge(submissions, on='globalid').ticket'''
         landings = pd.read_sql_table('landing_repeat', conn)
 
-
-    # Process landing data
+    # make flight IDs
     landing_flights = all_flights.loc[all_flights.submission_type == 'landings']\
         .dropna(axis=1, how='all') # columns for all track submissions will be empty so drop them
-    passengers = landings.melt(id_vars='parentglobalid',
-                               value_vars=[c for c in landings.columns if c.startswith('n_passengers')],
-                               value_name='n_passengers',
-                               var_name='landing_type')\
-        .dropna(subset=['n_passengers'])
-    #   calculate fees
-    fees = passengers.groupby('parentglobalid').sum() * LANDING_FEE
-    passengers.landing_type = passengers.landing_type.apply(lambda x: x.split('_')[-1])
-    #   make flight IDs and attach to landings and fees
-    landing_flights.rename(columns=LANDING_FLIGHT_COLUMNS, inplace=True)
-    landing_flights['flight_id'] = landing_flights.registration + '_' + landing_flights.departure_datetime.dt.floor('15min').dt.strftime()
+    landing_flights.rename(columns=LANDINGS_FLIGHT_COLUMNS, inplace=True)
+    landing_flights['flight_id'] = landing_flights.registration + '_' + \
+                                   landing_flights.departure_datetime.dt.floor('15min').dt.strftime('%Y%m%d%H%M')
     existing_flight_ids = pd.read_sql("SELECT flight_id FROM flights", landings_conn).squeeze(axis=1)
-    new_flights = landing_flights.loc[~landing_flights.flight_id.isin(existing_flight_ids)]
-    new_flights.to_sql('flights', conn, if_exists='append', index=False)
+    new_flights = landing_flights.loc[~landing_flights.flight_id.isin(existing_flight_ids) &
+                                      ~landing_flights.departure_datetime.isnull()]
+    if len(new_flights):
+        new_flights['submission_method'] = 'survey123'
+        new_flights['source_file'] = sqlite_path
+        new_flights.reindex(columns=LANDINGS_FLIGHT_COLUMNS.values())\
+            .to_sql('flights', landings_conn, if_exists='append', index=False)
+        global_ids = pd.read_sql("SELECT id, agol_global_id FROM flights WHERE flight_id IN ('%s')"
+                                 % "', '".join(new_flights.flight_id),
+                                 landings_conn)
 
-    #   insert
+        # Calculate concessions fees and attach numeric IDs to landings and fees
+        passengers = landings.melt(id_vars='parentglobalid',
+                                   value_vars=[c for c in landings.columns if c.startswith('n_passengers')],
+                                   value_name='n_passengers',
+                                   var_name='landing_type') \
+            .dropna(subset=['n_passengers'])
+        fees = passengers.groupby('parentglobalid').sum().reset_index()
+        fees['fee'] = fees.n_passengers * LANDING_FEE
+        fees['flight_id'] = fees.merge(global_ids, left_on='parentglobalid', right_on='agol_global_id').id
+        landings['flight_id'] = landings.merge(global_ids, left_on='parentglobalid', right_on='agol_global_id').id
+        passengers.landing_type = passengers.landing_type.apply(lambda x: x.split('_')[-1])
+        passengers['index'] = passengers.index # keep track of index so that duplicates are dropped on many-to-many merge
+        landings = passengers.merge(landings.drop(columns='landing_type'), on='parentglobalid')\
+            .drop_duplicates(subset='index')\
+            .rename(columns=LANDINGS_COLUMNS)\
+            .reindex(columns=LANDINGS_COLUMNS.values())
+
+        # INSERT into backend
+        fees.drop(columns='parentglobalid')\
+            .to_sql('concession_fees', landings_conn, if_exists='append', index=False)
+        landings\
+            .to_sql('landings', landings_conn, if_exists='append', index=False)
+
+
 
     # Send email to track editors
 
@@ -223,8 +253,17 @@ def main(log_dir, download_dir, agol_credentials_json, email_credentials_json, d
     Ensure that a log file is always written (unless the error occurs in write_log(), which is unlikely)
     '''
     #try:
-    # If I wrap this in a with engine.connect() as conn, conn.begin() block and pass, conn as an arg, then if I insert a submission record and the process fails before I can insert the flight data, the entire transaction will be rolled back
-    poll_feature_service(log_dir, download_dir, agol_credentials_json, email_credentials_json, db_credentials_json, ssl_cert)
+
+    # Open connections to the DBs and begin transactions so that if there's an exception, no data are inserted
+    with open(db_credentials_json) as j:
+        connection_info = json.load(j)
+    connection_template = 'postgresql://{username}:{password}@{ip_address}:{port}/{db_name}'
+    landings_engine = create_engine(connection_template.format(**connection_info['landings']))
+    tracks_engine = create_engine(connection_template.format(**connection_info['tracks']))
+
+    with landings_engine.connect() as l_conn, tracks_engine.connect() as t_conn, l_conn.begin(), t_conn.begin():
+        poll_feature_service(log_dir, download_dir, agol_credentials_json, email_credentials_json, db_credentials_json, ssl_cert, l_conn, t_conn)
+
     '''except Exception as error:
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         exc_type, exc_value, exc_traceback = sys.exc_info()
