7b74ce27b670d6e170542c2ac2e3a1edbf7717aa
smhooper <samhooperstudio@gmail.com>
2019-11-25 17:16:41 -0900

    write geojson to server

--- web/geojson_io.php
+++ web/geojson_io.php
@@ -1,12 +1,26 @@
 
 
 <?php
-//echo "Script called"
-if (isset($_POST["action"])) {
+
+if (isset($_POST['action'])) {
 	
-	if ($_POST["action"] == "getFiles") {
-		$json_files = array_filter(glob('*geojsons.json'));
+	// retrieve the names of all files that need to be edited
+	if ($_POST['action'] == 'getFiles') {
+		$json_files = array_filter(glob('data/*geojsons.json'));
 		echo json_encode($json_files);
 	}
+
+	// write json data to the server
+	if ($_POST['action'] == 'writeFile') {
+		// check that both the json string and the path to write the json to were given
+
+		if (isset($_POST['jsonString']) && isset($_POST['filePath'])) {
+			$success = file_put_contents($_POST['filePath'], $_POST['jsonString']);
+			echo $success;
+		} else {
+			echo false;
+		}
+	}//*/
 }
+
 ?>
