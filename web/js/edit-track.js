800f4d4872c868511e67f61841415753a057fa4d
smhooper <samhooperstudio@gmail.com>
2019-12-11 10:25:21 -0900

    split html, css, and js

--- web/js/edit-track.js
+++
@@ -1,8 +0,0 @@
-
-var editLine = function(newLineID) {
-    // Remove current points
-    lineSegmentLayers[currentLineID].markers.forEach(marker => map.removeLayer(marker))
-
-    // Add the new points
-    lineSegmentLayers[newLineID].markers.forEach(marker => marker.addTo(map)
-}
