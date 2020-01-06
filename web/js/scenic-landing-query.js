e2db83a97bc80a9fae77ba850d749bf672ae3907
smhooper <samhooperstudio@gmail.com>
2020-01-06 11:10:19 -0900

    show landings per flight;

--- web/js/scenic-landing-query.js
+++ web/js/scenic-landing-query.js
@@ -3,6 +3,20 @@
 //import * as flights from './flights.js';
 //const dataSteward = 'dena_flight_data@nps.gov';
 
+const flightColumns = [
+	'departure date',
+	'departure time',
+	'scenic route',
+	'total passengers',
+	'total fee'
+];
+const landingColumns = [
+	"landing location", 
+	"landing passengers", 
+	"landing type", 
+	"landing notes", 
+	"justification"
+];
 
 Date.prototype.addDays = function(days) {
     var date = new Date(this.valueOf());
@@ -16,6 +30,35 @@ Date.prototype.getChromeFormattedString = function() {
 	let month = '0' + (this.getMonth() + 1);
 	let day = '0' + (this.getDate())
 	return `${this.getFullYear()}-${month.slice(month.length - 2, month.length)}-${day.slice(day.length - 2, day.length)}`;
+}
+
+
+function fillSelectOptions(selectElementID, queryString, dbname, optionClassName='track-info-option') {
+    
+
+    var deferred = $.ajax({
+        url: 'geojson_io.php',
+        method: 'POST',
+        data: {action: 'query', dbname: dbname, queryString: queryString},
+        cache: false,
+        success: function(queryResultString){
+            var queryResult = queryResultString.startsWith('ERROR') ? false : $.parseJSON(queryResultString);
+            if (queryResult) {
+                queryResult.forEach(function(object) {
+                    $('#' + selectElementID).append(
+                        `<option class="${optionClassName}" value="${object.value}">${object.name}</option>`
+                    );
+                })
+            } else {
+                console.log(`error filling in ${selectElementID}: ${queryResultString}`);
+            }
+        },
+        error: function(xhr, status, error) {
+            console.log(`fill select failed with status ${status} because ${error} from query:\n${sql}`)
+        }
+    });
+
+    return deferred;
 }
 
 
@@ -34,16 +77,26 @@ function onRunClick(event) {
 	
 	const sql = `
 		SELECT 
-			flights.departure_datetime::date AS date, 
-			to_char(flights.departure_datetime, 'HH:MI AM') AS time, 
+			flights.id, 
+			flights.departure_datetime::date AS "departure date", 
+			to_char(flights.departure_datetime, 'HH:MI AM') AS "departure time", 
 			coalesce(flights.scenic_route, '') AS "scenic route",
-			concession_fees.n_passengers AS passengers, 
-			concession_fees.fee 
+			concession_fees.n_passengers AS "total passengers", 
+			concession_fees.fee AS "total fee", 
+			landing_locations.name AS "landing location", 
+			landings.n_passengers AS "landing passengers", 
+			landings.landing_type AS "landing type", 
+			coalesce(landings.notes, '') AS "landing notes", 
+			coalesce(landings.justification, '') AS justification  
 		FROM 
 			flights 
 		INNER JOIN 
 			concession_fees ON flights.id = concession_fees.flight_id 
-		WHERE
+		INNER JOIN 
+			landings ON flights.id = landings.flight_id 
+		INNER JOIN 
+			landing_locations ON landings.location = landing_locations.code 
+		WHERE 
 			flights.departure_datetime::date BETWEEN '${start_date}' AND '${end_date}' AND 
 			flights.operator_code = '${operator_code}'
 		; 
@@ -77,48 +130,133 @@ function onRunClick(event) {
 async function showQueryResult(result) {
 
 	$('#result-header-row').empty();
-	$('tbody').empty();
-	if (result.length) {
+	$('#place-holder').css('display', 'none');
+	$('#result-table-body').empty();
+	//$('#sum-row')
 
-	} else {
-		console.log('result was empty')
-		return;
+	// Organize the flights into an object with flight attributes and a landing (array) 
+	//	property that contains info about each landing
+	var flights = {};
+	for (i in result) {
+		let row = result[i];
+		let thisFlightID = row.id;
+		if (flights[thisFlightID] === undefined) {
+			flights[thisFlightID] = {};
+			for (i in flightColumns) {
+				let column = flightColumns[i];
+				flights[thisFlightID][column] = row[column];
+			}
+			flights[thisFlightID].landings = [];
+		}
+
+		//flights[thisFlightID] = {};
+
+
+		let thisLanding = {};
+		for (i in landingColumns) {
+			let column = landingColumns[i];
+			thisLanding[column] = row[column];
+		}
+		flights[thisFlightID].landings.push(thisLanding);
+
+		/*if (flights[thisFlightID].landings === undefined) {
+			flights[thisFlightID].landings = [thisLanding];
+		} else {
+			flights[thisFlightID].landings.push(thisLanding)
+		}*/
+
+		/*for (column in row) {
+			if (flightColumns.includes(column)) {
+				flights[thisFlightID][column] = row[column]
+			} else if (landingColumns.includes(column)) {
+				if (flights[thisFlightID].landings === undefined) flights[thisFlightID].landings = [];
+				let landingIndex = flights[thisFlightID].landings.length - 1;
+				let thisLanding = flights[thisFlightID].landings[landingIndex];
+				if (thisLanding === undefined) {
+					flights[thisFlightID].landings.push({});
+					landingIndex ++;
+				}
+				flights[thisFlightID].landings[landingIndex][column] = row[column];
+			}
+		}*/
+	}
+
+	var landingColumnRow = '';
+	for (i in landingColumns) {
+		let column = landingColumns[i]
+		let columnID = column.replace(' ', '_');
+		landingColumnRow += `<th class="landing-table-column-header" id="column-${columnID}">${column}</th> `
 	}
 
 	var columnIndex = 0;
 	var columnIndices = {};
 	var sums = {};
-	for (i in result) {
-		var thisFlight = result[i];
+	for (id in flights) {
+		var thisFlight = flights[id];
 		var tableCells = '';
-		for (column in thisFlight) {
+		for (i in flightColumns) {
+			let column = flightColumns[i];
 			let columnID = column.replace(' ', '_');
-			if (!$(`#th-${columnID}`).length) {
-				$(`<th id="th-${columnID}">${column}</th>`)
+			if (!$(`#column-${columnID}`).length) {
+				$(`<div class="result-table-column-header" id="column-${columnID}">${column}</div>`)
 					.appendTo('#result-header-row')
 				columnIndices[column] = columnIndex;
 				columnIndex ++;
 			}
 
 			var thisValue = thisFlight[column];
-			tableCells += `<td>${column === 'fee' ? '$' + parseFloat(thisValue).toFixed(2) : thisValue}</td>`
+			let cellValue = column === 'total fee' ? '$' + parseFloat(thisValue).toFixed(2) : thisValue;
+			tableCells += `<div class="result-table-cell cell-${columnID}">${cellValue}</div>`
 
 			thisValue = !isNaN(parseFloat(thisValue)) && isFinite(thisValue) ? parseFloat(thisValue) : '';
-			sums[column] = sums[column] === undefined ? thisValue : sums[column] + thisValue; 
+			sums[column] = sums[column] === undefined ? thisValue : sums[column] + thisValue;
 		}
-		$(`<tr>${tableCells}</tr>`).appendTo('#result-table > tbody')
+
+		let landingRows = '';
+		for (i in thisFlight.landings) {
+			let thisLanding = thisFlight.landings[i];
+			let thisRow = '';
+			for (column in thisLanding) {
+				let columnID = column.replace(' ', '_');
+				thisRow += `<td class="landing-table-cell cell-${columnID}">${thisLanding[column]}</td>`;
+			}
+			landingRows += `<tr>${thisRow}</tr>`;
+		}
+
+		$(`<div class="card" id="result-row-${id}"> 
+				<div class="card-header max-0 px-0" id="cardHeader-${id}" style="width:100%;">
+					<a class="collapsed card-link" data-toggle="collapse" href="#cardContent-${id}" style="width: 100%;">
+						<div class="row result-table-row">${tableCells}</div>
+					</a>
+				</div>
+				<div class="collapse" id="cardContent-${id}" aria-labeledby="cardHeader-${id}" data-parent="result-table-body" style="width: 100%; padding-left: 5%;">
+					<table style="width: 97%; float: right; margin-right: 2.5%; margin-top: 10px; margin-bottom: 30px;">
+						<thead>
+							<tr>${landingColumnRow}</tr>
+						</thead>
+						<tbody>
+							${landingRows}
+						</tbody>
+					</table>
+				</div>
+			</div>`
+		).appendTo('#result-table-body')
 	}
 
-	
 	var sumRow = '';
 	for (column in sums) {
+		let columnID = column.replace(' ', '_');
 		if (column.includes('passengers') || column.includes('fee') || column.includes('dropoff')){
-			sumRow += `<td>${column === 'fee' ? '$' + sums[column].toFixed(2) : sums[column]}</td>`;
+			sumRow += `<div class="result-table-cell cell-${columnID}">${column === 'total fee' ? '$' + sums[column].toFixed(2) : sums[column]}</div>`;
 		} else {
-			sumRow += '<td></td>'
+			sumRow += `<div class="result-table-cell cell-${columnID}"></div>`
 		}
+		
+		// Set the width for each cell to be the same as it's column
+		let columnWidth = $(`#column-${columnID}`).width();
+		$('#result-table-body').find(`.result-table-cell.cell-${columnID}`).css('width', columnWidth);
 	}
-	$(`<tr style="font-weight: bold; border-bottom: none;">${sumRow}</tr>`).appendTo('#result-table > tbody')	
+	$(`<div class="row result-table-row" id="sum-row" style="font-weight: bold; border-bottom: none; background-color: #8e5757e6;">${sumRow}</div>`).appendTo('#result-table-body')	
 
 
 
@@ -167,30 +305,3 @@ function hideLoadingIndicator(caller) {
 }
 
 
-function fillSelectOptions(selectElementID, queryString, dbname, optionClassName='track-info-option') {
-    
-
-    var deferred = $.ajax({
-        url: 'geojson_io.php',
-        method: 'POST',
-        data: {action: 'query', dbname: dbname, queryString: queryString},
-        cache: false,
-        success: function(queryResultString){
-            var queryResult = queryResultString.startsWith('ERROR') ? false : $.parseJSON(queryResultString);
-            if (queryResult) {
-                queryResult.forEach(function(object) {
-                    $('#' + selectElementID).append(
-                        `<option class="${optionClassName}" value="${object.value}">${object.name}</option>`
-                    );
-                })
-            } else {
-                console.log(`error filling in ${selectElementID}: ${queryResultString}`);
-            }
-        },
-        error: function(xhr, status, error) {
-            console.log(`fill select failed with status ${status} because ${error} from query:\n${sql}`)
-        }
-    });
-
-    return deferred;
-}
